642fee3fbf72c6577641c4e959b29dbcca0b853c
Emperor95 <emperoryba@gmail.com>
2019-05-24 16:53:57 +0000

    Add menu to ReadActivity

--- app/src/main/java/com/androidapk/diakonetapps/psycho_notes/ReadActivity.java
+++ app/src/main/java/com/androidapk/diakonetapps/psycho_notes/ReadActivity.java
@@ -2,86 +2,39 @@ package com.androidapk.diakonetapps.psycho_notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.util.FitPolicy;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
 
 public class ReadActivity extends AppCompatActivity {
 
-    PDFView view;
+    private PDFView pdfView;
     int position;
     int minPage, maxPage;
     ArrayList<Integer> allpages;
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
 
-//        if (getIntent().getExtras() != null){
-//            position = getIntent().getExtras().getInt("position");
-//
-//            switch (position){
-//                case 0:
-//                    minPage = 0;
-//                    maxPage = 1;
-//                    break;
-//
-//                case 1:
-//                    minPage = 2;
-//                    maxPage = 2;
-//                    break;
-//
-//                case 2:
-//                    minPage = 3;
-//                    maxPage = 5;
-//                    break;
-//
-//                case 3:
-//                    minPage = 6;
-//                    maxPage = 6;
-//                    break;
-//
-//                case 4:
-//                    minPage = 7;
-//                    maxPage = 18;
-//                    break;
-//
-//                case 5:
-//                    minPage = 19;
-//                    maxPage = 39;
-//                    break;
-//
-//                case 6:
-//                    minPage = 40;
-//                    maxPage = 64;
-//                    break;
-//
-//                case 7:
-//                    minPage = 65;
-//                    maxPage = 83;
-//                    break;
-//
-//            }
-//        }
-//
-//        int[] alPages = new int[maxPage - minPage];
-//
-//        for (int i = minPage; i <= maxPage; i++){
-//            for (int a = 0; a < 1; a++){
-//                alPages[a] = i;
-//            }
-//
-//        }
-
-        view = findViewById(R.id.view_pdf);
+        pdfView = findViewById(R.id.view_pdf);
 
 //        String filename = "psychology.pdf";
         String filename = "pro.pdf";
 
-        view.fromAsset(filename)
+        pdfView.fromAsset(filename)
                 .defaultPage(0)
                 .enableSwipe(true)
                 .swipeHorizontal(false)
@@ -94,5 +47,38 @@ public class ReadActivity extends AppCompatActivity {
                 .pageFling(false)
                 .load();
 
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener(){
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                mAdView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int i) {
+                super.onAdFailedToLoad(i);
+                Toast.makeText(ReadActivity.this, i + " Banner", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (R.id.night == item.getItemId()) {
+            pdfView.setNightMode(true);
+        } else {
+            Toast.makeText(ReadActivity.this, "Implementation underway ....", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
